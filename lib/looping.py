3703d3ea4aa41d5b5f8f4a0a60c944f0ba210fce
linder <achielinder@gmail.com>
2023-11-23 13:58:12 +0300

    completed assignments

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+  counter = 10
+  while counter > 0:
+      print(counter)
+      counter += 1
+  
 
 def square_integers(int_list):
     # code goes here!
     pass
+def square_integers(lst):
+    squared_lst = [x**2 for x in lst]
+    return squared_lst
 
 def fizzbuzz():
     # code goes here!
-    pass
+     for i in range(1, 101):
+       if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+       elif i % 3 == 0:
+            print("Fizz")
+       elif i % 5 == 0:
+             print("Buzz")
+       else:
+             print (i)
+  
